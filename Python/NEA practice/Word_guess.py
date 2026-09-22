bf04389f642db03c018ae8e991b43960423a6b6c
jawjab12 <136166477+jawjab12@users.noreply.github.com>
2026-09-22 19:16:17 +0100

    Enhance guessing logic and feedback in Word_guess.py
    Updated the check function to provide feedback on letter guesses and added logic to end the guessing after the maximum number of attempts.

--- Python/NEA practice/Word_guess.py
+++ Python/NEA practice/Word_guess.py
@@ -9,34 +9,40 @@ def spam_space():
     for i in range (50):
         print ("\n")
 
-def check (guess):
-    print ("Check") 
+def check(guess):
+    if guess in secret:
+        print("Correct letter!")
+    else:
+        print("Wrong letter!")
 
-
-
-    
 U1= input ("Player 1 name: ")
 U2= input ("Player 2 name: ")
 
-
-secret = input (U1+" ,please enter your secret word")
+secret = input (U1+" ,please enter your secret word: ")
 time.sleep (0.5)
 print ("hiding secret word")
 time.sleep (0.5)
 print ("...")
 spam_space()
 print ("continue from here:")
-while count > 3:
-    tries = False 
-
+xy=len(secret)
 while tries == True:
     guess = input (U2+" ,guess one letter for the word: ")
     count= (count +1)
-    print (check)
+    check(guess)
+    guess = ""
+    
+    
+    if count >= xy:
+        tries = False
     
 while tries == False:
+    spam_space()
     wordguess = input (U2+" ,guess  the word: ")
     if wordguess == secret:
-        print (U2+"Wins")
+        print (U2+" Wins")
     else:
-        print (U1+"Wins")
+        print (U1+" Wins")
+    
+    
+    tries = True
